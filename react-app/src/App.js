2b3cf343a40ef92caa475357c71c746f5a2ad0aa
pointatnick <nicholashma@gmail.com>
2016-12-13 18:19:35 -0800

    playing with references

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,33 +1,67 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      currentEvent: '---'
+      a: ''
     }
-    this.update = this.update.bind(this)
   }
   update(e) {
-    this.setState({currentEvent: e.type})
+    this.setState({
+      a: e.target.value,
+      b: this.refs.b.value,
+      c: this.c.value,
+      d: ReactDOM.findDOMNode(this.d).value,
+      e: this.e.refs.input.value
+    })
   }
   render() {
     return (
       <div>
-        <textarea
-          onKeyPress={this.update}
-          onCopy={this.update}
-          onCut={this.update}
-          onPaste={this.update}
-          onFocus={this.update}
-          onBlur={this.update}
-          onDoubleClick={this.update}
-          onTouchStart={this.update}
-          onTouchMove={this.update}
-          onTouchEnd={this.update}
-          cols="30"
-          rows="10" />
-        <h1>{this.state.currentEvent}</h1>
+        <input
+          type="text"
+          onChange={this.update.bind(this)}
+        /> {this.state.a}
+        <hr />
+        <input
+          ref="b"
+          type="text"
+          onChange={this.update.bind(this)}
+        /> {this.state.b}
+        <hr />
+        <input
+          ref={node => this.c = node}
+          type="text"
+          onChange={this.update.bind(this)}
+        /> {this.state.c}
+        <hr />
+        <Input
+          ref={component => this.d = component}
+          update={this.update.bind(this)}
+        /> {this.state.d}
+        <hr />
+        <DivInput
+          ref={component => this.e = component}
+          update={this.update.bind(this)}
+        /> {this.state.e}
+      </div>
+    )
+  }
+}
+
+class Input extends React.Component {
+  render() {
+    return <input type="text" onChange={this.props.update} />
+  }
+}
+
+class DivInput extends React.Component {
+  render() {
+    return (
+      <div>
+        <input ref="input" type="text" onChange={this.props.update} />
       </div>
     )
   }
