a6f0cf565ac32bb1120dfb47c52495863218acfe
pointatnick <nicholashma@gmail.com>
2016-12-13 16:37:16 -0800

    playing with props

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const App = () => <h1>Hello</h1>
+class App extends React.Component {
+  render() {
+    return <h1>{this.props.txt}</h1>
+  }
+}
+
+// const App = () => <h1>Hello Stateless</h1>
 
 export default App
